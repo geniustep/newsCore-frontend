9beff7d035c109d743e23fb9af046081d05b188c
geniustep <agsteps@gmail.com>
2025-12-03 00:17:11 +0100

    firest error

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,12 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { requestLocale } from 'next-intl/server';
 import { locales } from './config';
 
-export default getRequestConfig(async () => {
-  // Get locale using the new API
-  const locale = await requestLocale();
-  
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming locale parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
