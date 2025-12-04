382acee7a2ce5a84be109f4373d57916cff3e732
geniustep <agsteps@gmail.com>
2025-12-04 02:00:24 +0000

    fix erro

--- components/articles/BreakingNews.tsx
+++ components/articles/BreakingNews.tsx
@@ -49,8 +49,8 @@ export default function BreakingNews({ articles }: BreakingNewsProps) {
     }
   };
 
-  const timeAgo = current.published_at
-    ? formatDistanceToNow(new Date(current.published_at), {
+  const timeAgo = current.publishedAt
+    ? formatDistanceToNow(new Date(current.publishedAt), {
         addSuffix: true,
         locale: getLocale(),
       })
